2191292d08341abb6ee21d8f34cbf3cb42f4c76b
Arkadiy <alibullterrier@gmail.com>
2023-12-16 00:00:28 +0200

    new tasks

---
+++ codewars/5kyu/Integers Recreation One/IntegersRecreationOne.js
@@ -0,0 +1,16 @@
+const listSquared = (x, y) => {
+    let array = []
+    for(let i = x; i <= y; i++){
+      let result = 0
+      for(let j = 1; j <= i; j++){
+        if(i % j === 0){
+        result += j * j
+      }
+    }
+  if(Math.sqrt(result) % 1 === 0) {
+    array.push([i, result])
+  }
+  }
+  return array
+
+}
---
+++ codewars/8kyu/Transportation on vacation/Transportation on vacation.js
@@ -0,0 +1,5 @@
+const rentalCarCost = (d) => {
+  if(d >= 7) return d * 40 - 50
+  if(d >= 3) return d * 40 - 20
+  if(d < 3) return d * 40
+}
