43c03cb6e314bda4a364cc4dff5c1cc9d4c470de
Arkadiy <alibullterrier@gmail.com>
2024-01-16 00:00:38 +0200

    new tasks

---
+++ codewars/8kyu/101 Dalmatians - squash the bugs, not the dogs/101Dalmatians-SquashTheBugsNotTheDogs!.js
@@ -0,0 +1,12 @@
+const howManyDalmatians = (number) => {
+  const dogs = [
+    "Hardly any",
+    "More than a handful!",
+    "Woah that's a lot of dogs!",
+    "101 DALMATIANS!!!",
+  ];
+  if (number <= 10) return dogs[0];
+  if (number <= 50) return dogs[1];
+  if (number < 101) return dogs[2];
+  if ((number = 101)) return dogs[3];
+};
---
+++ codewars/8kyu/Determine offspring sex based on genes XX and XY chromosomes/DetermineOffspringSexBasedOnGenesXXAndXYChromosomes.js
@@ -0,0 +1,4 @@
+chromosomeCheck = (sperm) =>
+  sperm === "XX"
+    ? "Congratulations! You're going to have a daughter."
+    : "Congratulations! You're going to have a son.";
