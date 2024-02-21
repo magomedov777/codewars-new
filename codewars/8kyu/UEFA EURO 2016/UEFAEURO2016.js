6d3e5712fb909d1c124b8e179964bcbf01c15420
Arkadiy <alibullterrier@gmail.com>
2024-02-21 05:54:04 +0200

    new tasks

---
+++ codewars/8kyu/UEFA EURO 2016/UEFAEURO2016.js
@@ -0,0 +1,4 @@
+uefaEuro2016 = (teams, scores) =>
+  `At match ${teams[0]} - ${teams[1]}, ${
+    scores[0] == scores[1] ? "teams played draw." : `${scores[0] > scores[1] ? teams[0] : teams[1]} won!`
+  }`;
