18cb0a4b1793f279099286a1633ead0f04e8cfd5
Arkadiy <alibullterrier@gmail.com>
2024-03-04 05:37:59 +0200

    new tasks

---
+++ codewars/8kyu/Flick Switch/FlickSwitch.js
@@ -0,0 +1,11 @@
+function flickSwitch(arr) {
+  let result = [];
+  let switchValue = false;
+  for (let item of arr) {
+    if (item === "flick") {
+      switchValue = !switchValue;
+    }
+    result.push(!switchValue);
+  }
+  return result;
+}
